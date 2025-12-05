07863ab497ac6589b685ec55d3bf66bb4c691690
Randsafi <rands068@gmail.com>
2025-12-05 15:19:50 +0200

    updat3

---
+++ src/layout/layout.js
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router";
+import "./layout.css";
+// import NavBar from "../components/navbar/navbar";
+import CourseDetailsNavBar from "../components/course-details-components/course-details-navbar/course-details-navbar";
+const Layout = () => {
+
+    return <div className="pages_styles">
+        {/* <NavBar /> */}.
+        <CourseDetailsNavBar />
+        <Outlet />
+    </div>
+}
+export default Layout;
+
