289d994a61d94eacd2c89c71bdccac25334b47e2
Randsafi <rands068@gmail.com>
2025-12-26 23:37:56 +0200

    save problem navbar

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,18 +1,15 @@
 import { Outlet } from "react-router";
 import "./layout.css";
 import Navbar from "../components/volunteer-projects/Navbar";
-// import NavBar from "../components/navbar/navbar";
-// import CourseDetailsNavBar from "../components/course-details-components/course-details-navbar/course-details-navbar";
+
 const Layout = () => {
 
-    return <div className="pages_styles">
-        {/* <NavBar /> */}
+    return <div>
 
         <Navbar />
-
-        {/* <CourseDetailsNavBar /> */}
-
+    <div className="pages_styles">
         <Outlet />
+    </div>
     </div>
 }
 export default Layout;
