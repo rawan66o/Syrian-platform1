af26c4fb478702a7c5f8d0302840e51f9ef25ef3
msatat-23 <nour.assi.2025@gmail.com>
2025-12-28 14:38:53 +0300

    fix navbar

--- src/components/course-details-components/course-details-navbar/course-details-navbar.js
+++ src/components/course-details-components/course-details-navbar/course-details-navbar.js
@@ -1,42 +1,100 @@
 import "./course-details-navbar.css";
 import { NavLink } from "react-router";
-
+import { useLocation } from "react-router";
 const CourseDetailsNavBar = () => {
+    const location = useLocation();
+    const loggedIn = true;
+    const fullNavPaths = [
+        "/",
+        "/courses",
+        "/courses/:courseId",
+        "/volunteer-projects",
+        "/student-guide",
+        "/coach-guide",
+        "/volunteer-guide",
+        "/partners",
+        "/dashboard",
+        "/dashboard/profile",
+        "/dashboard/certificates",
+        "/dashboard/my-courses",
+        "/dashboard/my-progects",
+    ];
 
-    return <div className="course_details_navbar">
-        <div className="nav_body">
-            <div className="nav_body_icon_links">
-                <div className="nav_icon_container">
-                    <img className="nav_icon_img" src="/icons/syrian_platform_icon/syrian_platform.svg" alt="" />
+
+
+    if (fullNavPaths.includes(location.pathname)) {
+        return <div className="course_details_navbar">
+            <div className="nav_body" dir="rtl">
+                <div className="nav_body_icon_links">
+                    <div className="nav_icon_container">
+                        <img className="nav_icon_img" src="/icons/syrian_platform_icon/syrian_platform.svg" alt="" />
+                    </div>
+                    <div className="nav_separation_line" />
+                    <div className="nav_links_to_pages">
+                        <NavLink className="home_nav_link">الرئيسية</NavLink>
+                        <NavLink className="course_nav_link">الكورسات</NavLink>
+                        <NavLink className="course_nav_link">المنتدى</NavLink>
+                        <NavLink className="course_nav_link">المشاريع</NavLink>
+                        <NavLink className="course_nav_link">اتصل بنا</NavLink>
+                    </div>
                 </div>
-                <div className="nav_separation_line" />
-                <div className="nav_links_to_pages">
-                    <NavLink className="home_nav_link">Home</NavLink>
-                    <NavLink className="course_nav_link">Projects</NavLink>
-                    <NavLink className="course_nav_link">Forum</NavLink>
-                    <NavLink className="course_nav_link">Courses</NavLink>
-                    <NavLink className="course_nav_link">Contact</NavLink>
+                <div className="course_details_navbar_search_container">
+                    <input className="course_details_nav_search_input" type="search" placeholder="البحث" />
+                    <img className="course_details_nav_search_icon" src="/icons/search_icon/search_normal.svg" alt="" />
                 </div>
-            </div>
-            <div className="course_details_navbar_search_container">
-                <input className="course_details_nav_search_input" type="search" placeholder="Research" />
-                <img className="course_details_nav_search_icon" src="/icons/search_icon/search_normal.svg" alt="" />
-            </div>
-            <div className="signup_notifications_language_container">
-                <div className="notifications_language_container">
-                    <button className="not_lang_button">
-                        <img src="/icons/language_icon/material-symbols-light_language.svg" alt="" />
-                    </button>
-                    <button className="not_lang_button">
-                        <img src="/icons/notifications_icon/notification.svg" alt="" />
-                    </button>
-                </div>
-                <div className="nav_separation_line" />
-                <div className="signup_button_container">
-                    <button className="signup_button">Sign Up</button>
+                <div className="signup_notifications_language_container">
+                    <div className="notifications_language_container">
+                        <button className="not_lang_button">
+                            <img src="/icons/notifications_icon/notification.svg" alt="" />
+                        </button>
+                        <button className="not_lang_button">
+                            <img src="/icons/language_icon/material-symbols-light_language.svg" alt="" />
+                        </button>
+                    </div>
+                    <div className="nav_separation_line" />
+                    <div className="signup_button_container">
+                        {!loggedIn && <button className="signup_button">تسجيل الدخول</button>}
+                        {loggedIn && <div className="navbar_user_container">
+                            <img src="/images/nav_user/nav_user.png" alt="" />
+                            <p className="navbar_user_container_username">المدرب.محمد الشيخ</p>
+                        </div>}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    }
+    else {
+        return <div className="course_details_navbar">
+            <div className="nav_body" dir="rtl">
+                <div className="nav_body_icon_links">
+                    <div className="nav_icon_container">
+                        <img className="nav_icon_img" src="/icons/syrian_platform_icon/syrian_platform.svg" alt="" />
+                    </div>
+                    <div className="nav_separation_line" />
+                    <div className="nav_links_to_pages">
+                        <NavLink className="home_nav_link">الرئيسية</NavLink>
+                    </div>
+                </div>
+                <div className="signup_notifications_language_container">
+                    <div className="notifications_language_container">
+                        <button className="not_lang_button">
+                            <img src="/icons/notifications_icon/notification.svg" alt="" />
+                        </button>
+                        <button className="not_lang_button">
+                            <img src="/icons/language_icon/material-symbols-light_language.svg" alt="" />
+                        </button>
+                    </div>
+                    <div className="nav_separation_line" />
+                    <div className="signup_button_container">
+                        {!loggedIn && <button className="signup_button">تسجيل الدخول</button>}
+                        {loggedIn && <div className="navbar_user_container">
+                            <img src="/images/nav_user/nav_user.png" alt="" />
+                            <p className="navbar_user_container_username">المدرب.محمد الشيخ</p>
+                        </div>}
+                    </div>
+                </div>
+            </div>
+        </div>
+    }
 };
 export default CourseDetailsNavBar;
--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router";
 import "./layout.css";
-// import NavBar from "../components/navbar/navbar";
+import NavBar from "../components/navbar/navbar";
 import CourseDetailsNavBar from "../components/course-details-components/course-details-navbar/course-details-navbar";
+import { useLocation } from "react-router";
 const Layout = () => {
+    const location = useLocation();
+    const authPaths = ["/register", "/login", "/forgot-password", "new-password"];
 
+    console.log(location)
     return <div className="pages_styles">
-        {/* <NavBar /> */}
-        <CourseDetailsNavBar />
+        {authPaths.includes(location.pathname) ? <NavBar /> :
+            <CourseDetailsNavBar />}
         <Outlet />
     </div>
 }
